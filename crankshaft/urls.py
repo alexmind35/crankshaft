67b7f62b87d15124382820fbf74e9a4d07797b11
Александр Михайлов <38732070+alexmind35@users.noreply.github.com>
2020-03-17 19:28:23 +0300

    Получилось!!!

--- crankshaft/urls.py
+++ crankshaft/urls.py
@@ -14,19 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
-
-from services import views
 
 urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
-
-    path('mine/', views.ManageCourseListView.as_view(), name='manage_course_list'),
-    path('create/', views.CourseCreateView.as_view(), name='course_create'),
-    path('<pk>/edit/', views.CourseUpdateView.as_view(), name='course_edit'),
-    path('<pk>/delete/', views.CourseDeleteView.as_view(), name='course_delete'),
+    path('course/', include('services.urls')),
 
 ]

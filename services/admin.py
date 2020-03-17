5e64ea556f1e6737b7eab1b5cb83f3c41b78498c
Александр Михайлов <38732070+alexmind35@users.noreply.github.com>
2020-03-17 10:37:03 +0300

    Подготовка к makemigrations

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Service
 
-# Register your models here.
+
+@admin.register(Service)
+class AdminService(admin.ModelAdmin):
+    list_display = ["name", "price"]

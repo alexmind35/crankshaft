b095c85415dcefae0beb838246173c26795ceb7e
Александр Михайлов <38732070+alexmind35@users.noreply.github.com>
2020-03-17 09:41:11 +0300

    До makemigrations

---
+++ services/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Service(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField("Название", max_length=250)
+    price = models.IntegerField()
+
+    class Meta:
+        verbose_name = "Услуги"
+        verbose_name_plural = "Услуги"
+
